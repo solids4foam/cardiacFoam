aeccda3a89282957e2378f044744efb71e6ba6e7
Philip Cardiff <philipcardiff@gmail.com>
2023-02-18 21:50:20 +0000

    Add activationTime field

--- applications/solvers/electroActivationFoam/electroActivationFoam.C
+++ applications/solvers/electroActivationFoam/electroActivationFoam.C
@@ -113,6 +113,40 @@ int main(int argc, char *argv[])
 
         if (runTime.writeTime())
         {
+            // Update activationTimes field (used in Niederer benchmark)
+            const scalarField& VmI = Vm.primitiveFieldRef();
+            const scalarField& VmOldI = Vm.oldTime().primitiveFieldRef();
+            scalarField& activationTimeI = activationTime.primitiveFieldRef();
+            const scalar oldTime = runTime.value() - runTime.deltaTValue();
+            forAll(activationTimeI, cellI)
+            {
+                if (calculateActivationTime[cellI])
+                {
+                    if (VmI[cellI] > SMALL)
+                    {
+                        calculateActivationTime[cellI] = false;
+
+                        // Use current time
+                        // activationTimeI[cellI] = runTime.value();
+                        if (VmOldI[cellI] > SMALL)
+                        {
+                            // How does this scenario occur?
+                            activationTimeI[cellI] = oldTime;
+                        }
+                        else
+                        {
+                            // Linearly interpolate for more accuracy
+                            const scalar w =
+                                (0.0 - VmOldI[cellI])
+                               /(VmI[cellI] - VmOldI[cellI]);
+
+                            activationTimeI[cellI] =
+                                oldTime + w*runTime.deltaTValue();
+                        }
+                    }
+                }
+            }
+
             runTime.write();
         }
 
