24c6b855333ad215fe085e86109e14b2b9a7aaa9
Aaron Mullen-Hales <aaronmullen-hales@Aarons-MacBook-Pro.local>
2025-05-08 18:30:14 +0100

    Adding the Petsc solution to problem1

---
+++ tutorials/LandEtAl2015/problem1/Petsc/extractDz.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Output file
+output="dz_values.dat"
+echo "# mesh dz" > "$output"
+
+# Loop through mesh2 to mesh5
+for mesh in mesh2 mesh3 mesh4 mesh5; do
+    file="$mesh/postProcessing/0/solidPointDisplacement_pointDisp.dat"
+    
+    if [[ -f "$file" ]]; then
+        # Get the dz value: 4th entry of 2nd line
+        dz=$(awk 'NR==2 { print $4 }' "$file")
+        echo "$mesh $dz" >> "$output"
+    else
+        echo "Warning: $file not found"
+    fi
+done
