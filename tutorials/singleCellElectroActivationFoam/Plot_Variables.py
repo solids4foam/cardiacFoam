b952c87b59a649c8572e2d90fc5f2367ca39597b
Simão Castro <simaocastro@Simaos-MacBook-Pro.local>
2025-06-17 15:27:35 +0100

    adding post processing files

---
+++ tutorials/singleCellElectroActivationFoam/Plot_Variables.py
@@ -0,0 +1,165 @@
+
+
+##The matrix is in time with variables per line  - 17 states - 66 algebraic - 17 rates
+
+# legend_states[0] = "V in component membrane (millivolt)"
+# legend_states[1] = "K_i in component potassium_dynamics (millimolar)"
+# legend_states[2] = "Na_i in component sodium_dynamics (millimolar)"
+# legend_states[3] = "Ca_i in component calcium_dynamics (millimolar)"
+# legend_states[4] = "Xr1 in component rapid_time_dependent_potassium_current_Xr1_gate (dimensionless)"
+# legend_states[5] = "Xr2 in component rapid_time_dependent_potassium_current_Xr2_gate (dimensionless)"
+# legend_states[6] = "Xs in component slow_time_dependent_potassium_current_Xs_gate (dimensionless)"
+# legend_states[7] = "m in component fast_sodium_current_m_gate (dimensionless)"
+# legend_states[8] = "h in component fast_sodium_current_h_gate (dimensionless)"
+# legend_states[9] = "j in component fast_sodium_current_j_gate (dimensionless)"
+# legend_states[10] = "d in component L_type_Ca_current_d_gate (dimensionless)"
+# legend_states[11] = "f in component L_type_Ca_current_f_gate (dimensionless)"
+# legend_states[12] = "fCa in component L_type_Ca_current_fCa_gate (dimensionless)"
+# legend_states[13] = "s in component transient_outward_current_s_gate (dimensionless)"
+# legend_states[14] = "r in component transient_outward_current_r_gate (dimensionless)"
+# legend_states[15] = "Ca_SR in component calcium_dynamics (millimolar)"
+# legend_states[16] = "g in component calcium_dynamics (dimensionless)"
+
+# legend_algebraic[0] = "i_Stim in component membrane (picoA_per_picoF)"
+# legend_algebraic[1] = "xr1_inf in component rapid_time_dependent_potassium_current_Xr1_gate (dimensionless)"
+# legend_algebraic[2] = "xr2_inf in component rapid_time_dependent_potassium_current_Xr2_gate (dimensionless)"
+# legend_algebraic[3] = "xs_inf in component slow_time_dependent_potassium_current_Xs_gate (dimensionless)"
+# legend_algebraic[4] = "m_inf in component fast_sodium_current_m_gate (dimensionless)"
+# legend_algebraic[5] = "h_inf in component fast_sodium_current_h_gate (dimensionless)"
+# legend_algebraic[6] = "j_inf in component fast_sodium_current_j_gate (dimensionless)"
+# legend_algebraic[7] = "d_inf in component L_type_Ca_current_d_gate (dimensionless)"
+# legend_algebraic[8] = "f_inf in component L_type_Ca_current_f_gate (dimensionless)"
+# legend_algebraic[9] = "alpha_fCa in component L_type_Ca_current_fCa_gate (dimensionless)"
+# legend_algebraic[10] = "s_inf in component transient_outward_current_s_gate (dimensionless)"
+# legend_algebraic[11] = "r_inf in component transient_outward_current_r_gate (dimensionless)"
+# legend_algebraic[12] = "g_inf in component calcium_dynamics (dimensionless)"
+# legend_algebraic[13] = "E_Na in component reversal_potentials (millivolt)"
+# legend_algebraic[14] = "alpha_xr1 in component rapid_time_dependent_potassium_current_Xr1_gate (dimensionless)"
+# legend_algebraic[15] = "alpha_xr2 in component rapid_time_dependent_potassium_current_Xr2_gate (dimensionless)"
+# legend_algebraic[16] = "alpha_xs in component slow_time_dependent_potassium_current_Xs_gate (dimensionless)"
+# legend_algebraic[17] = "alpha_m in component fast_sodium_current_m_gate (dimensionless)"
+# legend_algebraic[18] = "alpha_h in component fast_sodium_current_h_gate (per_millisecond)"
+# legend_algebraic[19] = "alpha_j in component fast_sodium_current_j_gate (per_millisecond)"
+# legend_algebraic[20] = "alpha_d in component L_type_Ca_current_d_gate (dimensionless)"
+# legend_algebraic[21] = "tau_f in component L_type_Ca_current_f_gate (millisecond)"
+# legend_algebraic[22] = "beta_fCa in component L_type_Ca_current_fCa_gate (dimensionless)"
+# legend_algebraic[23] = "tau_s in component transient_outward_current_s_gate (millisecond)"
+# legend_algebraic[24] = "tau_r in component transient_outward_current_r_gate (millisecond)"
+# legend_algebraic[25] = "d_g in component calcium_dynamics (per_millisecond)"
+# legend_algebraic[26] = "E_K in component reversal_potentials (millivolt)"
+# legend_algebraic[27] = "beta_xr1 in component rapid_time_dependent_potassium_current_Xr1_gate (dimensionless)"
+# legend_algebraic[28] = "beta_xr2 in component rapid_time_dependent_potassium_current_Xr2_gate (dimensionless)"
+# legend_algebraic[29] = "beta_xs in component slow_time_dependent_potassium_current_Xs_gate (dimensionless)"
+# legend_algebraic[30] = "beta_m in component fast_sodium_current_m_gate (dimensionless)"
+# legend_algebraic[31] = "beta_h in component fast_sodium_current_h_gate (per_millisecond)"
+# legend_algebraic[32] = "beta_j in component fast_sodium_current_j_gate (per_millisecond)"
+# legend_algebraic[33] = "beta_d in component L_type_Ca_current_d_gate (dimensionless)"
+# legend_algebraic[34] = "gama_fCa in component L_type_Ca_current_fCa_gate (dimensionless)"
+# legend_algebraic[35] = "E_Ks in component reversal_potentials (millivolt)"
+# legend_algebraic[36] = "tau_xr1 in component rapid_time_dependent_potassium_current_Xr1_gate (millisecond)"
+# legend_algebraic[37] = "tau_xr2 in component rapid_time_dependent_potassium_current_Xr2_gate (millisecond)"
+# legend_algebraic[38] = "tau_xs in component slow_time_dependent_potassium_current_Xs_gate (millisecond)"
+# legend_algebraic[39] = "tau_m in component fast_sodium_current_m_gate (millisecond)"
+# legend_algebraic[40] = "tau_h in component fast_sodium_current_h_gate (millisecond)"
+# legend_algebraic[41] = "tau_j in component fast_sodium_current_j_gate (millisecond)"
+# legend_algebraic[42] = "gamma_d in component L_type_Ca_current_d_gate (millisecond)"
+# legend_algebraic[43] = "fCa_inf in component L_type_Ca_current_fCa_gate (dimensionless)"
+# legend_algebraic[44] = "E_Ca in component reversal_potentials (millivolt)"
+# legend_algebraic[45] = "tau_d in component L_type_Ca_current_d_gate (millisecond)"
+# legend_algebraic[46] = "d_fCa in component L_type_Ca_current_fCa_gate (per_millisecond)"
+# legend_algebraic[47] = "alpha_K1 in component inward_rectifier_potassium_current (dimensionless)"
+# legend_algebraic[48] = "beta_K1 in component inward_rectifier_potassium_current (dimensionless)"
+# legend_algebraic[49] = "xK1_inf in component inward_rectifier_potassium_current (dimensionless)"
+# legend_algebraic[50] = "i_K1 in component inward_rectifier_potassium_current (picoA_per_picoF)"
+# legend_algebraic[51] = "i_Kr in component rapid_time_dependent_potassium_current (picoA_per_picoF)"
+# legend_algebraic[52] = "i_Ks in component slow_time_dependent_potassium_current (picoA_per_picoF)"
+# legend_algebraic[53] = "i_Na in component fast_sodium_current (picoA_per_picoF)"
+# legend_algebraic[54] = "i_b_Na in component sodium_background_current (picoA_per_picoF)"
+# legend_algebraic[55] = "i_CaL in component L_type_Ca_current (picoA_per_picoF)"
+# legend_algebraic[56] = "i_b_Ca in component calcium_background_current (picoA_per_picoF)"
+# legend_algebraic[57] = "i_to in component transient_outward_current (picoA_per_picoF)"
+# legend_algebraic[58] = "i_NaK in component sodium_potassium_pump_current (picoA_per_picoF)"
+# legend_algebraic[59] = "i_NaCa in component sodium_calcium_exchanger_current (picoA_per_picoF)"
+# legend_algebraic[60] = "i_p_Ca in component calcium_pump_current (picoA_per_picoF)"
+# legend_algebraic[61] = "i_p_K in component potassium_pump_current (picoA_per_picoF)"
+# legend_algebraic[62] = "i_rel in component calcium_dynamics (millimolar_per_millisecond)"
+# legend_algebraic[63] = "i_up in component calcium_dynamics (millimolar_per_millisecond)"
+# legend_algebraic[64] = "i_leak in component calcium_dynamics (millimolar_per_millisecond)"
+# legend_algebraic[65] = "Ca_i_bufc in component calcium_dynamics (dimensionless)"
+# legend_algebraic[66] = "Ca_sr_bufsr in component calcium_dynamics (dimensionless)"
+
+# legend_rates[0] = "d/dt V in component membrane (millivolt)"
+# legend_rates[1] = "d/dt K_i in component potassium_dynamics (millimolar)"
+# legend_rates[2] = "d/dt Na_i in component sodium_dynamics (millimolar)"
+# legend_rates[3] = "d/dt Ca_i in component calcium_dynamics (millimolar)"
+# legend_rates[4] = "d/dt Xr1 in component rapid_time_dependent_potassium_current_Xr1_gate (dimensionless)"
+# legend_rates[5] = "d/dt Xr2 in component rapid_time_dependent_potassium_current_Xr2_gate (dimensionless)"
+# legend_rates[6] = "d/dt Xs in component slow_time_dependent_potassium_current_Xs_gate (dimensionless)"
+# legend_rates[7] = "d/dt m in component fast_sodium_current_m_gate (dimensionless)"
+# legend_rates[8] = "d/dt h in component fast_sodium_current_h_gate (dimensionless)"
+# legend_rates[9] = "d/dt j in component fast_sodium_current_j_gate (dimensionless)"
+# legend_rates[10] = "d/dt d in component L_type_Ca_current_d_gate (dimensionless)"
+# legend_rates[11] = "d/dt f in component L_type_Ca_current_f_gate (dimensionless)"
+# legend_rates[12] = "d/dt fCa in component L_type_Ca_current_fCa_gate (dimensionless)"
+# legend_rates[13] = "d/dt s in component transient_outward_current_s_gate (dimensionless)"
+# legend_rates[14] = "d/dt r in component transient_outward_current_r_gate (dimensionless)"
+# legend_rates[15] = "d/dt Ca_SR in component calcium_dynamics (millimolar)"
+# legend_rates[16] = "d/dt g in component calcium_dynamics (dimensionless)"
+
+#-________________________________________________-----_________________________________-
+
+# Choose the column index you want to plot (0 is time)
+
+#Note: Algebraic = n + 18, Rates = n + 67
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the data
+file_path = 'output.txt'
+df = pd.read_csv(file_path, delim_whitespace=True)
+
+# print(df.iloc[0])
+# df.head(1).to_csv('first_line.csv', index=False)
+
+num_columns = df.shape[1]
+
+print("Number of columns:", num_columns)
+
+# Columns to plot
+cols_to_plot = [1,4,71,75,69,70,73,68] 
+
+y_labels = [
+    "Voltage mV",
+    "Ca_i (mM)",
+    "i_Na (pA/pF)",
+    "i_to (pA/pF)",
+    "i_Kr (pA/pF)",
+    "i_Ks (pA/pF)",
+    "i_CaL (pA/pF)",
+    "i_K1 (pA/pF)"
+   
+]  # customize
+
+
+
+time = df.iloc[:, 0]  # time is first column
+# Create subplots: one row per variable, 1 column
+# Create a 4x2 grid of subplots
+fig, axes = plt.subplots(4, 2, figsize=(12, 12), sharex=True)
+axes = axes.flatten()  # flatten axes 1D ARRAY
+
+for ax, col_id, y_label in zip(axes, cols_to_plot, y_labels):
+    ax.plot(time, df.iloc[:, col_id])
+    ax.set_ylabel(y_label)
+    ax.grid(True)
+
+# Set x-label only on the bottom plots (the last row)
+for ax in axes[-2:]:
+    ax.set_xlabel("time (ms)")
+
+plt.tight_layout()
+plt.show()
+
+
+
