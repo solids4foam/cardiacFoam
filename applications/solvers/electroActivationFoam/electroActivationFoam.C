3df48126c8ee7b9f78ba105a5150218e3020e6d8
Philip Cardiff <philip.cardiff@ucd.ie>
2025-06-23 17:44:40 +0100

    Add run-time selectable ionicModels

---
+++ applications/solvers/electroActivationFoam/electroActivationFoam.C
@@ -0,0 +1,131 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+    Copyright (C) 2019 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    electroActivationFoam
+
+Description
+    Solves the reaction-diffusion equation for muscle electrophysiology
+    stemming from the mono-domain approach, where the ionic model is run-time
+    selectable.
+
+Authors
+    Philip Cardiff, UCD.
+    Simão Nieto de Castro, UCD.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "ionicModel.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createFields.H"
+
+    // Loop through time
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.loop())
+    {
+        Info<< nl << "Time = " << runTime.timeName() << endl;
+
+        // Set the external stimulus current for this time
+        scalarField& externalStimulusCurrentI = externalStimulusCurrent;
+        externalStimulusCurrentI = 0.0;
+        if (runTime.value() <= stimulusDuration.value())
+        {
+            forAll(stimulusCellIDs, cI)
+            {
+                const label cellID = stimulusCellIDs[cI];
+
+                externalStimulusCurrentI[cellID] += stimulusIntensity.value();
+            }
+        }
+        externalStimulusCurrent.correctBoundaryConditions();
+
+        // Solve the ionic model given the current voltage and calculate the
+        // ionic model currents
+        scalarField& ionicCurrentI = ionicCurrent;
+        ionicCurrentI = 0.0;
+        ionicModel->calculateCurrent
+        (
+            runTime.value() - runTime.deltaTValue(),
+            runTime.deltaTValue(),
+            Vm.internalField(),
+            ionicCurrentI
+        );
+        ionicCurrent.correctBoundaryConditions();
+
+        // Construct and solve the voltage equation given a known ionic current
+        // and  external stimulus current
+        fvScalarMatrix VmEqn
+        (
+            beta*Cm*fvm::ddt(Vm)
+         ==
+            fvm::laplacian(conductivity, Vm)
+          - BuenoOrovioScaleFactor*beta*Cm*ionicCurrent // GET RID OF THIS SCALE FACTOR!
+          + externalStimulusCurrent
+        );
+
+        VmEqn.solve();
+
+#       include "updateActivationTimes.H"
+
+        if (runTime.writeTime())
+        {
+            // Update the activation velocity
+            activationVelocity =
+                fvc::grad
+                (
+                    1.0
+                   /(
+                       activationTime
+                     + dimensionedScalar("SMALL", dimTime, SMALL)
+                    )
+                );
+
+            runTime.write();
+
+            runTime.printExecutionTime(Info);
+        }
+    }
+
+    Info<< nl << endl;
+
+    runTime.printExecutionTime(Info);
+
+    Info<< "End" << nl << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
---
+++ src/ionicModels/ionicModel/ionicModel.C
@@ -0,0 +1,93 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2025 AUTHOR,AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "ionicModel.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(ionicModel, 0);
+    defineRunTimeSelectionTable(ionicModel, dictionary);
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::ionicModel::ionicModel
+(
+    const dictionary& dict, const label num, const scalar initialDeltaT
+)
+:
+    ODESystem(),
+    odeSolver_(),
+    dict_(dict),
+    step_(num, initialDeltaT)
+{}
+
+
+// * * * * * * * * * * * * * * * * Selectors * * * * * * * * * * * * * * * * //
+
+Foam::autoPtr<Foam::ionicModel>
+Foam::ionicModel::New
+(
+    const dictionary& dict,
+    const label nIntegrationPoints,
+    const scalar initialDeltaT
+)
+{
+    const word modelType(dict.lookup("type"));
+
+    Info<< "Selecting ionic model " << modelType << endl;
+
+    auto* ctorPtr = dictionaryConstructorTable(modelType);
+
+    if (!ctorPtr)
+    {
+        FatalIOErrorInLookup
+        (
+            dict,
+            "ionicModel",
+            modelType,
+            *dictionaryConstructorTablePtr_
+        )   << exit(FatalIOError);
+    }
+
+    return autoPtr<ionicModel>
+    (
+        ctorPtr(dict, nIntegrationPoints, initialDeltaT)
+    );
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::ionicModel::~ionicModel()
+{}
+
+
+// ************************************************************************* //
