ed759f0b2cee2906b37b4bcd02f3eb5e56d13643
Simao Castro <simao.castro@ucdconnect.ie>
2026-05-22 11:09:59 +0100

    solve header files mismatch called by git

--- src/electroModels/electroDomains/extracellularPotentialDomain/extracellularPotentialDomain.C
+++ src/electroModels/electroDomains/extracellularPotentialDomain/extracellularPotentialDomain.C
@@ -1,6 +1,7 @@
 /*---------------------------------------------------------------------------*\
 License
     This file is part of cardiacFoam.
+
     cardiacFoam is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by the
     Free Software Foundation, either version 3 of the License, or (at your
@@ -13,6 +14,7 @@ License
 
     You should have received a copy of the GNU General Public License
     along with cardiacFoam.  If not, see <http://www.gnu.org/licenses/>.
+
 \*---------------------------------------------------------------------------*/
 
 #include "extracellularPotentialDomain.H"
