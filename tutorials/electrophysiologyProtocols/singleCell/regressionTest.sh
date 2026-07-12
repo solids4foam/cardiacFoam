95114d661ae2ac0d4feede2395d6fa342ff1c01b
Simao Castro <simao.castro@ucdconnect.ie>
2026-07-12 17:02:39 +0100

    Revert "tweak(singleCell): enable Vm debug output, loosen regression tolerance"
    This reverts commit 3dd1314eb79d941f41698dd8d66c0e18cdb754e3.

--- tutorials/electrophysiologyProtocols/singleCell/regressionTest.sh
+++ tutorials/electrophysiologyProtocols/singleCell/regressionTest.sh
@@ -63,7 +63,7 @@ while IFS=' ' read -r fileName time variable expected tolerance; do
                 }
             }
             END {
-                if (found && bestDiff <= 1e-4) {
+                if (found && bestDiff <= 1e-9) {
                     print actual;
                     exit 0;
                 }
